b35d9296cda471c06c451fe8463060d1747f6e5f
Tinara Nathania <tinaranathania@gmail.com>
2022-06-23 16:21:25 +0700

    fix: change the background color of OnboardingView

--- Fergie/OnBoardingView.swift
+++ Fergie/OnBoardingView.swift
@@ -11,37 +11,42 @@ struct OnboardingView: View {
     @State var isLinkActive = false
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    Spacer()
-                    Image("fergieDancing")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 300, height: 300)
-                    Spacer()
-                }
-                Spacer()
-                    .frame(height: 20)
-                Text("Meet Fergie the Cloud.").fontWeight(.semibold)
-                Spacer()
-                    .frame(height: 10)
-                Text("Let's cheer you on doing your daily tasks!")
-                Spacer()
-                    .frame(height: 70)
+            ZStack {
+                Color.ui.gray
+                    .ignoresSafeArea()
 
-                NavigationLink(destination: ContentView(), isActive: $isLinkActive) {
-                    Button(action: {
-                        self.isLinkActive = true
-                    }) {
-                        Text("Get Started")
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image("fergieDancing")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                        Spacer()
                     }
+                    Spacer()
+                        .frame(height: 20)
+                    Text("Meet Fergie the Cloud.").fontWeight(.semibold)
+                    Spacer()
+                        .frame(height: 10)
+                    Text("Let's cheer you on doing your daily tasks!")
+                    Spacer()
+                        .frame(height: 70)
+
+                    NavigationLink(destination: ContentView(), isActive: $isLinkActive) {
+                        Button(action: {
+                            self.isLinkActive = true
+                        }) {
+                            Text("Get Started")
+                        }
+                    }
+                    .padding(.horizontal, 25)
+                    .padding(.vertical, 10)
+                    .background(Color.ui.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    .frame(maxWidth: .infinity, alignment: .center)
                 }
-                .padding(.horizontal, 25)
-                .padding(.vertical, 10)
-                .background(Color.ui.blue)
-                .foregroundColor(.white)
-                .clipShape(Capsule())
-                .frame(maxWidth: .infinity, alignment: .center)
             }
         }
     }
